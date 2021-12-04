461ba19468aaec3c007d6ff5fce53922749f6b0c
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2021-12-04 15:50:50 -0300

    Added h1 and two buttons
    Added h1 whit description of task.
    Added one button that can add a member.
    Added one button that can remove a member.

--- Tarea 2/tarea-2.js
+++ Tarea 2/tarea-2.js
@@ -1,7 +1,34 @@
 /*
 TAREA:
-Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
-Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
+Crear una interfaz que permita agregar ó quitar (botones agregar y quitar)
+inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
+
+Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual,
+menor salario anual, salario anual promedio y salario mensual promedio.
 
 Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
 */
+
+const $cuerpoHtml = document.querySelector("body");
+const $form = document.querySelector("form");
+
+document.querySelector("#add-member").onclick = function () {
+  const $newInput = document.createElement("input");
+  $newInput.setAttribute("type", "number");
+  $newInput.setAttribute("id", "members-annual-salary");
+  $newInput.setAttribute("placeholder", "Annual salary");
+  $form.appendChild($newInput);
+  const $newLabel = document.createElement("label");
+  $newLabel.className = "label-class";
+  $newLabel.setAttribute("text", "annual salary");
+  $newInput.appendChild($newLabel);
+  return false;
+};
+
+const $buttonRemoveMember = document.getElementById("remove-member");
+
+$buttonRemoveMember.onclick = function () {
+  let $dad = document.getElementById("members-salary");
+  let $children = document.getElementById("members-annual-salary");
+  let deleteChildren = $dad.removeChild($children);
+};
